9f368ebc3f2743df3b0ac54ec64f2fb63dd85368
vamshi <vamshikistareddy@gmail.com>
2024-07-17 22:05:52 +0530

    Added extra line

--- authproject/authproject/urls.py
+++ authproject/authproject/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from testapp import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_view),
